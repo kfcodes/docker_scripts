aa38fdec3a950877c85fc865ee58ea1e4fe691e8
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-10-27 22:09:35 +0100

    Changed the zoneminder image that is used for the container

--- zoneminder.sh
+++ zoneminder.sh
@@ -1,13 +1,5 @@
-#! bin/bash
-
 docker run -d \
-	--name zoneminder \
-	-p 8443:443/tcp \
-	-p 9000:9000/tcp \
-	-e TZ Europe/Dublin \
-	-e PUID 99 \
-	-e PGID 100 \
-	-v /mnt/Zoneminder:/config\
-	-v /mnt/Zoneminder/data:/var/cache/zoneminder \
-	dlandon/zoneminder
-
+-t \
+-p 1080:443 \
+--name zoneminder \
+zoneminderhq/zoneminder:latest-el7
