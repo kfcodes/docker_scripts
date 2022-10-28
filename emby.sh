8c1306b64dffda5d85ae6cc9b5e1a4ad2bd7ecbb
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-10-28 20:20:15 +0100

    Added user and group id's to emby container

--- emby.sh
+++ emby.sh
@@ -1,7 +1,10 @@
 #! bin/bash
 
+# Added user and group ids to emby container
 docker run -d \
 --name=emby \
+-e PUID=1000 \
+-e PGID=1000 \
 -e TZ=Europe/Dublin \
 --restart unless-stopped \
 lscr.io/linuxserver/emby:latest
