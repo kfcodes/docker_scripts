36da73a9a9ccc83e773963ba806f2af3e00afa1b
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-10-29 22:05:28 +0100

    Added port configuration to emby container

--- emby.sh
+++ emby.sh
@@ -1,10 +1,11 @@
 #! bin/bash
 
-# Added user and group ids to emby container
+# Added port number to emby container
 docker run -d \
 --name=emby \
 -e PUID=1000 \
 -e PGID=1000 \
 -e TZ=Europe/Dublin \
+-p 8096:8096 \
 --restart unless-stopped \
 lscr.io/linuxserver/emby:latest
