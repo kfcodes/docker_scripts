fe80abcd5f7ebec7c0066a81507488ffb23676cd
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-10-29 22:06:35 +0100

    Added storage volums to emby container

--- emby.sh
+++ emby.sh
@@ -1,11 +1,13 @@
 #! bin/bash
 
-# Added port number to emby container
 docker run -d \
 --name=emby \
 -e PUID=1000 \
 -e PGID=1000 \
 -e TZ=Europe/Dublin \
 -p 8096:8096 \
+-v ~/emby/config:/config \
+-v ~/emby/tvshows:/data/tvshows \
+-v ~/emby/movies:/data/movies \
 --restart unless-stopped \
 lscr.io/linuxserver/emby:latest
