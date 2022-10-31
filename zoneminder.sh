817466dd4ff7a00868d2b70ee382d6ec8306b9ba
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-10-31 19:32:28 +0000

    Update the config for zoneminder to include ports and user id

--- zoneminder.sh
+++ zoneminder.sh
@@ -1,5 +1,12 @@
-docker run -d \
--t \
--p 1080:443 \
---name zoneminder \
+docker run -d \ 
+--name="zoneminder" \
+--privileged="false" \
+--shm-size="5G" \
+-p 8443:443/tcp \
+-p 9000:9000/tcp \
+-e TZ="Europe/Dublin" \
+-e PUID="99" \
+-e PGID="100" \
+-v "/mnt/zoneminder":"/config":rw \
+-v "/mnt/zoneminder/data":"/var/cache/zoneminder":rw \
 zoneminderhq/zoneminder:latest-el7
