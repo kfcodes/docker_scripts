81c66ff5603db542b15e309d8b55306de04cc687
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-01-29 20:28:13 +0000

    Added dvb devies to tvheadend docker container

--- tvheadend.sh
+++ tvheadend.sh
@@ -7,5 +7,6 @@ docker run -d \
  -p 9982:9982 \
  -v ~/dockerData/tvheadend/config:/config:Z \
  -v ~/dockerData/tvheadend/recordings:/recordings:Z \
+ --device /dev/dvb:/dev/dvb \
  --restart unless-stopped \
 linuxserver/tvheadend
