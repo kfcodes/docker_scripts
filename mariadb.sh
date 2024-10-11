b2788dc359786c18cb79bd4af9a1d7db971f47be
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-10-11 10:24:46 +0100

    Updated the mariadb docker container

--- mariadb.sh
+++ mariadb.sh
@@ -1,8 +1,9 @@
 docker run -d \
-    --name mariadb \
-    -p 127.0.0.1:3306:3306 \
+    --name mariadb3 \
+    -p 127.0.0.1:3307:3306 \
     --env MARIADB_USER=test\
     --env MARIADB_PASSWORD=test\
     --env MARIADB_ROOT_PASSWORD=root_password \
+    --restart unless-stopped \
+    --network app_network
     mariadb:latest
-    # -v ~/.mysql_scripts:/docker-entrypoint-initdb.d/ \
