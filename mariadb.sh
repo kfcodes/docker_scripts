0440eaeef8fc4f704ea461445897bffd115f8db7
kfcodes <+kfcodes@users.noreply.github.com>
2024-12-09 18:03:16 +0000

    Updated port for mariadb docker container

--- mariadb.sh
+++ mariadb.sh
@@ -1,9 +1,9 @@
 docker run -d \
-    --name mariadb3 \
-    -p 127.0.0.1:3307:3306 \
+    --name mariadb \
+    -p 127.0.0.1:3306:3306 \
     --env MARIADB_USER=test\
     --env MARIADB_PASSWORD=test\
     --env MARIADB_ROOT_PASSWORD=root_password \
     --restart unless-stopped \
-    --network app_network
+    --network app_network \
     mariadb:latest
